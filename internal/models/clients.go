dd9199abfe5894e29afb82c47a43627cf990529b
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-11 12:33:04 +0500

    with a clean slate

--- internal/models/clients.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Clients struct {
-	ID          uint   `json:"id"`
-	Fio         string `json:"fio"`
-	Email       string `json:"email"`
-	PhoneNumber string `json:"phoneNumber"`
-}
--- internal/models/test.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Test struct {
-	Id     int    `json:"id"`
-	Status string `json:"status"`
-	Email  string `json:"email"`
-	Fio    string `json:"fio"`
-}
