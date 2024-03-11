789052d0e10a7f98c6f7fdb5c067ab2d13eb5c05
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-11 14:02:57 +0500

    create models Crequests

---
+++ database/models/crequests_model.go
@@ -0,0 +1,11 @@
+package models
+
+type Crequests struct {
+	ID           uint   `json:"id"`
+	FIO          string `json:"fio"`
+	Email        string `json:"email"`
+	PhoneNumber  string `json:"phoneNumber"`
+	Status       bool   `json:"status"`
+	StorageData  string `json:"storageData"`
+	StoragePrice string `json:"storagePrice"`
+}
