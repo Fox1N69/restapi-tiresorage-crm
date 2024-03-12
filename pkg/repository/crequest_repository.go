56d095f82be7acce71d5e7676cafb697fb4c41de
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-12 14:12:35 +0500

    create crequest repository

--- pkg/repository/crequest_repository.go
+++ pkg/repository/crequest_repository.go
@@ -6,13 +6,23 @@ import (
 	"gorm.io/gorm"
 )
 
-type CrequestRepositoryI interface {
-	CreateCrequst(crequest models.Crequests) error
-}
 type CrequestRepository struct {
 	db *gorm.DB
 }
 
+type CrequestRepositoryI interface {
+	CreateCrequest(crequest models.Crequests) error
+	GetCrequestByID(id uint) (*models.Crequests, error)
+}
+
 func NewCrequestRepository(db *gorm.DB) *CrequestRepository {
 	return &CrequestRepository{}
+}
+
+func (cr *CrequestRepository) CreateCrequest(crequest models.Crequests) error {
+	return nil
+}
+
+func (cr *CrequestRepository) GetCrequestByID(id uint) (*models.Crequests, error) {
+	return nil, nil
 }
