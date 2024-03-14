1e8501d280bc5c287d94645b5fedcb9943e2bdda
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-14 22:10:40 +0500

    create func getallcrequest

--- pkg/handlers/crequest_handler.go
+++ pkg/handlers/crequest_handler.go
@@ -1,9 +1,19 @@
 package handlers
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"crud-crm/pkg/database"
+	"crud-crm/pkg/models"
+
+	"github.com/gofiber/fiber/v3"
+)
 
 func (h *Handler) GetAllCrequests(c fiber.Ctx) error {
-	return nil
+	var data models.Crequests
+	if err := database.DB.Find(&data).Error; err != nil {
+		return err
+	}
+
+	return c.JSON(data)
 }
 
 func (h *Handler) CreateCrequest(c fiber.Ctx) error {
