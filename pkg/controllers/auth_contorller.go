7fe5a17dc238d3aeda4ec90d6fc521dbc08dfb2c
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-17 13:49:32 +0500

    added auth controller

--- pkg/controllers/auth_contorller.go
+++ pkg/controllers/auth_contorller.go
@@ -1,11 +1,32 @@
 package controllers
 
-import "gorm.io/gorm"
+import (
+	"github.com/gofiber/fiber/v3"
+	"gorm.io/gorm"
+)
 
 type AuthController struct {
 	DB *gorm.DB
 }
 
+type AuthControllerI interface {
+	Login(c fiber.Ctx) error
+	Register(c fiber.Ctx) error
+	Logout(c fiber.Ctx) error
+}
+
 func NewAuthController(db *gorm.DB) *AuthController {
 	return &AuthController{DB: db}
+}
+
+func (ac *AuthController) Login(c fiber.Ctx) error {
+	return nil
+}
+
+func (ac *AuthController) Register(c fiber.Ctx) error {
+	return nil
+}
+
+func (ac *AuthController) Logout(c fiber.Ctx) error {
+	return nil
 }
