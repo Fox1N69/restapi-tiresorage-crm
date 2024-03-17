d255205fcd734a203f8f83781929532940bcf792
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-17 14:34:38 +0500

    The login function on auth_controller.go has been remove

--- pkg/controllers/auth_contorller.go
+++ pkg/controllers/auth_contorller.go
@@ -1,7 +1,6 @@
 package controllers
 
 import (
-	"crud-crm/pkg/database"
 	"crud-crm/pkg/models"
 	"encoding/json"
 
@@ -24,50 +23,9 @@ func NewAuthController(db *gorm.DB) *AuthController {
 	return &AuthController{DB: db}
 }
 
-func (ac *AuthController) GenerateToken(c fiber.Ctx) error {
-	var user models.User
-	if err := ac.DB.Where("username = ?", c.FormValue("username")).First(&user).Error; err != nil {
-		return err
-	}
-
-	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(c.FormValue("password"))); err != nil {
-		return fiber.ErrUnauthorized
-	}
-
-	return c.JSON(fiber.Map{"token": ""})
-}
-
-
-func (ac *AuthController) issueToken(userID uint) (string, error) {
-	token, err := ac.GenerateToken(userID)
-	if err != nil {
-		return "", err
-	}
-	return token, nil
-}
 
 func (ac *AuthController) Login(c fiber.Ctx) error {
-	var user models.User
-
-	if err := json.Unmarshal(c.Body(), &user); err != nil {
-		return err
-	}
-
-	err := ac.DB.Raw("SELECT password FROM users WHERE username = ? LIMIT 1", user.Username).Row().Scan(&user.Password)
-	if err != nil {
-		return err
-	}
-
-	if err := bcrypt.CompareHashAndPassword(user.Password, user.Password); err != nil {
-		return err
-	}
-
-	token, err := ac.issueToken(user.ID)
-	if err != nil {
-		return err
-	}
-
-	return c.JSON(fiber.Map{"token": token})
+	retuner nil
 }
 
 func (ac *AuthController) Register(c fiber.Ctx) error {
