49a1a9572f2436ab28e62e36fe3c2ee0f12a075e
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-18 09:32:01 +0500

    change auth_repository

--- pkg/repository/auth_repository.go
+++ pkg/repository/auth_repository.go
@@ -1,14 +1,44 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"crud-crm/pkg/models"
+
+	"gorm.io/gorm"
+)
 
 type AuthRepository struct {
 	DB *gorm.DB
 }
 
 type AuthRepositoryI interface {
+	CreateUser(user *models.User) error
+	GetUserByID(id uint) (models.User, error)
+	UpdateUser(user *models.User) error
+	DeleteUser(user *models.User) error
 }
 
 func NewAuthReposit(db *gorm.DB) *AuthRepository {
 	return &AuthRepository{DB: db}
+}
+
+func (ar *AuthRepository) CreateUser(user *models.User) error {
+	return nil
+}
+
+func (ar *AuthRepository) GetUserByID(id uint) (models.User, error) {
+	var user models.User
+
+	if err := ar.DB.Where("id = ?", id).First(&user).Error; err != nil {
+		return models.User{}, err
+	}
+
+	return user, nil
+}
+
+func (ar *AuthRepository) UpdateUser(user *models.User) error {
+	return ar.DB.Save(user).Error
+}
+
+func (ar *AuthRepository) DeleteUser(user *models.User) error {
+	return ar.DB.Delete(user).Error
 }
