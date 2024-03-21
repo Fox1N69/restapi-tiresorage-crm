f6821391cbd74bb995d8fa85e6ffa3bb058e3f74
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-21 20:13:54 +0500

    create verifyToken function and generate token func

--- pkg/controllers/auth_contorller.go
+++ pkg/controllers/auth_contorller.go
@@ -2,9 +2,10 @@ package controllers
 
 import (
 	"crud-crm/pkg/models"
+	"time"
 
 	"github.com/gofiber/fiber/v3"
-	"golang.org/x/crypto/bcrypt"
+	"github.com/golang-jwt/jwt/v4"
 	"gorm.io/gorm"
 )
 
@@ -16,12 +17,17 @@ type AuthControllerI interface {
 	Login(c fiber.Ctx) error
 	Register(c fiber.Ctx) error
 	Logout(c fiber.Ctx) error
-	PasswordCrypted(c fiber.Ctx, user *models.User) (error, []byte)
+	GenerateToken(user *models.User) (string, error)
+	VerifyToken(tokenString string) (*jwt.Token, error)
 }
 
 func NewAuthController(db *gorm.DB) *AuthController {
 	return &AuthController{DB: db}
 }
+
+const (
+	secretKey = "secret"
+)
 
 func (ac *AuthController) Login(c fiber.Ctx) error {
 	return nil
@@ -35,7 +41,29 @@ func (ac *AuthController) Logout(c fiber.Ctx) error {
 	return nil
 }
 
-func (ac *AuthController) PasswordCrypted(c fiber.Ctx, user *models.User) (error, []byte) {
-	password, _ := bcrypt.GenerateFromPassword([]byte(user.Password), 14)
-	return nil, password
+// Generate jwt token
+func (ac *AuthController) GenerateToken(user *models.User) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["user_id"] = user.ID
+	claims["username"] = user.Username
+
+	claims["exp"] = time.Now().Add(time.Hour * 1).Unix()
+
+	tokenString, err := token.SignedString([]byte(secretKey))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func (ac *AuthController) VerifyToken(tokenString string) (*jwt.Token, error) {
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, jwt.ErrSignatureInvalid
+		}
+		return []byte(secretKey), nil
+	})
 }
